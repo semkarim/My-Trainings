3494d597ace8b586061c8ef2f0df7d5d148a0158
semkarim <65163745+semkarim@users.noreply.github.com>
2020-05-13 19:31:09 +0300

    Finished seq.py

---
+++ Data structures/seq.py
@@ -0,0 +1,22 @@
+shoplist = ['apples', 'mango', 'carrot', 'bananas']
+name = 'ANAROG'
+
+#Действия над списком
+print('Element 0 -', shoplist[0])
+print('Element 1 -', shoplist[1])
+print('Element 2 -', shoplist[2])
+print('Element 3 -', shoplist[3])
+print('Element -1 -', shoplist[-1])
+print('Element -2 -', shoplist[-2])
+print('Element -3 -', shoplist[-3])
+print('Element -4 -', shoplist[-4])
+#Действия над словами
+print(name[0])
+print(name[:2])
+print(name[2:])
+print(name[1:3])
+print(name[1:-2])
+print(name[:])
+#Действия над словом в списке (можно применить любую команду выше)
+print(shoplist[0][2:4])
+
